ae973dace9b1d98f68c47adfa7c62be09aec2225
haloz <809219376@qq.com>
2019-08-26 12:51:11 +0000

    finish getWeather api

---
+++ Exceptions/HttpException.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * This file is part of the haloz/weather.
+ *
+ * (c) haloz <809219376@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Haloz\Weather\Exception;
+
+class HttpException extends Exception
+{
+}
